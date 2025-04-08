1180a7a2c6b2ddc9dc7925f0d348b8c1dead3e09
Ellen Ruan <eruan@umich.edu>
2025-04-08 19:03:03 -0400

    about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,18 @@
 import Footer from "../ui/components/footer";
 
-export default function Page() {
+export default function About() {
     return (
-        <>
-            <div>
-                <p>Hello World</p>
-            </div>
-        </>
+        /*About Title*/
+        <div className="z-20 px-6">
+          <h1
+            className="text-7xl md:text-8xl font-bold leading-tight text-black"
+            style={{ textShadow: "0 2px 4px rgba(0, 0, 0, 0.6)" }}
+          >
+            About
+          </h1>
+        </div>
+
+        /*Who we are*/
+        
     )
 }
