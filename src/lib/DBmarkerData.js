c5e5e274388d23013bbb248040ff20da6e967716
connorjd1 <connorjd@umich.edu>
2025-10-22 18:56:09 -0400

    working on dual maps

---
+++ src/lib/DBmarkerData.js
@@ -0,0 +1,20 @@
+export const DBmarkerData = [
+  {
+    title: "Tony England Engineering Lab Building",
+    address: "2383 Richard Dr, Dearborn, MI 48128",
+    pv: "204.18 kWDC, 0.2 MW",
+    system: "Rooftop PV",
+  },
+  {
+    title: "testg",
+    address: "2183 Richard Dr, Dearborn, MI 48128",
+    pv: "204.18 kWDC, 0.2 MW",
+    system: "Rooftop PV",
+  },
+  {
+    title: "test",
+    address: "2823 Richard Dr, Dearborn, MI 48128",
+    pv: "204.18 kWDC, 0.2 MW",
+    system: "Rooftop PV",
+  },
+];
