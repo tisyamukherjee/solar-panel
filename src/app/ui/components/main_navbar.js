5f02868db8ef49afeab47a297b2e163e8b0d921c
Tisya Mukherjee <tisyam@umich.edu>
2025-11-05 18:51:21 -0500

    changed name of website in header

--- src/app/ui/components/main_navbar.js
+++ src/app/ui/components/main_navbar.js
@@ -6,31 +6,31 @@ export default function Navbar() {
     return (
         <header className="bg-[#00274C]">
             {/* <div className="max-w-5xl flex items-center justify-between px-6 py-4 font-montserrat"> */}
-            <div className="w-full flex items-center px-6 py-4 font-Merriweather">
+            <div className="w-full flex items-center px-6 py-3 font-Merriweather">
                 {/* Logo */}
                 <div className="flex flex-row gap-5 items-center"> 
                     <Link href="/">
                         <Image 
                             src="/blockM.png"
                             alt="UM logo"
-                            width="80"
-                            height="80"
+                            width="50"
+                            height="50"
                         />
                     </Link>
-                    <div className="h-15 w-[2px] bg-white"></div>
+                    <div className="h-10 w-[2px] bg-white"></div>
                     <div className="text-center">
-                        <h1 className="text-4xl text-white font-Merriweather font-bold text-center"> EcoData x SfCE </h1> 
-                        <h2 className="text-white font-Merriweather font-bold text-center"> University of Michigan</h2>
+                        <h1 className="text-2xl text-white font-montserrat font-bold text-center"> Maize Rays </h1> 
+                        {/* <h2 className="text-white font-Merriweather font-bold text-center"> University of Michigan</h2> */}
                     </div>
                 </div>
 
                 {/* Nav links */}
-                <nav className="ml-auto text-lg text-white font-['IBM Plex Sans Devanagri'] divide-x divide-white/100 flex flex-row">
-                    <Link href="/" className="hover:text-yellow-400 transition px-6">Home</Link>
-                    <Link href="/map" className="hover:text-yellow-400 transition px-6">Map</Link>
-                    <Link href="/education" className="hover:text-yellow-400 transition px-6">Get Educated</Link>
+                <nav className="ml-auto text-lg text-white font-['IBM Plex Sans Devanagri'] font-semibold divide-x divide-white/100 flex flex-row">
+                    <Link href="/" className="hover:text-yellow-400 transition px-5">Home</Link>
+                    <Link href="/map" className="hover:text-yellow-400 transition px-5">Map</Link>
+                    <Link href="/education" className="hover:text-yellow-400 transition px-5">Get Educated</Link>
                     {/* <Link href="/contact" className="hover:text-yellow-400 transition px-6">Contact</Link> */}
-                    <Link href="/team" className="hover:text-yellow-400 transition px-6">Team</Link>
+                    <Link href="/team" className="hover:text-yellow-400 transition px-5">Team</Link>
                     <LoginLogoutButton/>
                 </nav>
             </div>
