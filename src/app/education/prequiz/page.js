d6ab83f5baa13e0e2e25fd006b3b80ffaaf5a018
Tisya Mukherjee <tisyam@umich.edu>
2025-11-19 17:34:41 -0500

    added pre and post quiz

--- src/app/education/prequiz/page.js
+++ src/app/education/prequiz/page.js
@@ -42,17 +42,17 @@ const questions = [
 
 export default function PreQuiz() {
     return(
-        <div className="bg-[#00274C] font-montserrat flex flex-col items-center h-full overflow-y-scroll gap-10">
-            <h1 className="text-white text-8xl">Pre-Quiz</h1>
-            <h2 className="text-[#FFCB05] text-4xl"> Test your knowledge on solar energy! </h2>
+        <div className="font-montserrat flex flex-col h-full overflow-y-scroll gap-10">
+            <h1 className="text-[#00274C] text-7xl text-center font-medium mt-5">Pre-Quiz</h1>
+            <h2 className="text-black text-4xl text-center"> Test your knowledge on solar energy! </h2>
             {/* questions */}
-            <div className="bg-white h-fit w-200 flex items-center flex-col rounded-4xl gap-5 p-10">
+            <div className="bg-white h-fit w-full flex flex-col rounded-4xl gap-10 mb-15">
                 {questions.map((q) => (
                     <div key={q.question} className="flex flex-col gap-5">
-                        <span className="text-[#000080] text-4xl font-bold border-5 px-30 py-5 border-solid border-[#FFCB05] rounded-4xl">{q.question}</span>
+                        <span className="text-[#000080] text-2xl font-bold border-5 px-10 py-5 border-solid border-[#FFCB05] rounded-4xl">{q.question}</span>
                         <div>
                             {q.answers.map((a, index) => (
-                                <div key={index} className="text-lg">
+                                <div key={index} className="text-xl m-3">
                                 <input 
                                     type="radio" 
                                     id={a} 
@@ -65,7 +65,7 @@ export default function PreQuiz() {
                             </div>
                     </div>
                 ))}
-            <button className="bg-[#00274C] text-white py-4 px-5 rounded-4xl"> Submit </button>
+            <button className="bg-[#00274C] text-white text-xl py-4 px-5 rounded-4xl hover:text-yellow-400 cursor-pointer"> Submit </button>
             </div>
         </div>
     );
